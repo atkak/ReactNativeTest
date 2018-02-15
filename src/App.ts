e5179f936166e1d5bcb6d8fe9716b5d9bdcf9e98
hotchemi <hot.chemistry21@gmail.com>
2018-02-15 13:33:53 +0900

    Fix to activate type definition.

--- src/App.ts
+++ src/App.ts
@@ -1,7 +1,6 @@
 import { AppRegistry } from 'react-native';
 import Root from './root';
-// import { AWS } from 'aws-sdk';
-const AWS = require('aws-sdk/dist/aws-sdk-react-native');
+import AWS = require('aws-sdk');
 
 export default function start() {
   AWS.config.credentials = new AWS.CognitoIdentityCredentials({ IdentityPoolId: 'ap-northeast-1:9494aea9-9873-466b-bb4f-85f52c7a0b02' });
